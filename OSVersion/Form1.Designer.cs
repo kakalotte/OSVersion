5636e7709bf1f4a5b8d5fea1d1b95d44c8352b1e
mastermind <cyberdude@mail.ru>
2016-04-21 02:16:10 +0300

    +

---
+++ OSVersion/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace OSVersion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OSInfo_TextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OSInfo_TextBox
+            // 
+            this.OSInfo_TextBox.Location = new System.Drawing.Point(12, 12);
+            this.OSInfo_TextBox.Multiline = true;
+            this.OSInfo_TextBox.Name = "OSInfo_TextBox";
+            this.OSInfo_TextBox.ReadOnly = true;
+            this.OSInfo_TextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.OSInfo_TextBox.Size = new System.Drawing.Size(602, 349);
+            this.OSInfo_TextBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 373);
+            this.Controls.Add(this.OSInfo_TextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Environment Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox OSInfo_TextBox;
+    }
+}
+
---
+++ OSVersion/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net.NetworkInformation;
+
+namespace OSVersion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // добавить инфу о процессоре, пямяти, видеокарте
+
+            var info = "";
+
+            info += "OS Info:" + Environment.NewLine;
+            info += string.Format("Name = {0}", OSInfo.Name) + Environment.NewLine;
+            info += string.Format("Edition = {0}", OSInfo.Edition) + Environment.NewLine;
+            info += string.Format("Service Pack = {0}", OSInfo.ServicePack) + Environment.NewLine;
+            info += string.Format("Version = {0}", OSInfo.VersionString) + Environment.NewLine;
+            info += string.Format("Bits = {0}", OSInfo.OSBits) + Environment.NewLine + Environment.NewLine;
+
+            info += GetNetworkInterfacesInfo();
+
+            OSInfo_TextBox.Text = info;
+        }
+
+        public static string GetNetworkInterfacesInfo()
+        {
+            var info = "";
+
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            info += string.Format("Network Interfaces information for {0}:     ",
+                    computerProperties.HostName + (computerProperties.DomainName == ""? "" : "." + computerProperties.DomainName)) + Environment.NewLine;
+            if (nics == null || nics.Length < 1)
+            {
+                info += string.Format("  No network interfaces found.");
+                return info;
+            }
+
+            info += string.Format("  Interfaces count .................... : {0}", nics.Length) + Environment.NewLine + Environment.NewLine;
+            foreach (NetworkInterface adapter in nics)
+            {
+                var test = adapter.GetIPProperties();
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+
+                info += string.Format(adapter.Description) + Environment.NewLine;
+                info += string.Format(String.Empty.PadLeft(adapter.Description.Length, '=')) + Environment.NewLine;
+                info += string.Format("  Interface type .......................... : {0}", adapter.NetworkInterfaceType) + Environment.NewLine;
+                info += string.Format("  Physical Address ........................ : {0}",
+                           adapter.GetPhysicalAddress().ToString()) + Environment.NewLine;
+
+
+
+                foreach (UnicastIPAddressInformation ip in adapter.GetIPProperties().UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        info += string.Format("  IP Address ........................ : {0}", ip.Address.ToString()) + Environment.NewLine;
+                    }
+                }
+
+
+
+                info += string.Format("  Operational status ...................... : {0}",
+                    adapter.OperationalStatus) + Environment.NewLine;
+                string versions = "";
+
+                // Create a display string for the supported IP versions.
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    versions = "IPv4";
+                }
+                if (adapter.Supports(NetworkInterfaceComponent.IPv6))
+                {
+                    if (versions.Length > 0)
+                    {
+                        versions += " ";
+                    }
+                    versions += "IPv6";
+                }
+                info += string.Format("  IP version .............................. : {0}", versions) + Environment.NewLine;
+
+
+                // The following information is not useful for loopback adapters.
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                {
+                    continue;
+                }
+                //info += string.Format("  DNS address .............................. : {0}",
+                //    properties.DnsAddresses) + Environment.NewLine;
+                info += "  DNS addresses .............................. : ";
+                //var test1 = properties.DnsAddresses[0];
+                //var t2 = test1.ToString();
+                for (int i = 0; i < properties.DnsAddresses.Count; i++)
+                {
+                    info += properties.DnsAddresses[i].ToString();
+
+                    if (i != properties.DnsAddresses.Count - 1)
+                        info += ", ";
+                    else
+                        info += Environment.NewLine;
+                }
+
+
+                string label;
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    IPv4InterfaceProperties ipv4 = properties.GetIPv4Properties();
+                    
+                    if(ipv4 != null)
+                    {
+                        info += string.Format("  MTU...................................... : {0}", ipv4.Mtu) + Environment.NewLine;
+                        if (ipv4.UsesWins)
+                        {
+
+                            IPAddressCollection winsServers = properties.WinsServersAddresses;
+                            if (winsServers.Count > 0)
+                            {
+                                label = "  WINS Servers ............................ :";
+                            }
+                        }
+                    }
+                }
+
+                info += string.Format("  DNS enabled ............................. : {0}",
+                    properties.IsDnsEnabled) + Environment.NewLine;
+                info += string.Format("  Dynamically configured DNS .............. : {0}",
+                    properties.IsDynamicDnsEnabled) + Environment.NewLine;
+                info += string.Format("  Receive Only ............................ : {0}",
+                    adapter.IsReceiveOnly) + Environment.NewLine;
+                info += string.Format("  Multicast ............................... : {0}",
+                    adapter.SupportsMulticast) + Environment.NewLine;
+
+                info += Environment.NewLine;
+            }
+
+            return info;
+        }
+    }
+}
